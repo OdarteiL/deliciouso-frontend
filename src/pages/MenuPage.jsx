f7657070be427678ebe9420e49eb5bd22cef4da6
Odartei Lamptey <odarteilamptey.ml@gmail.com>
2024-02-02 17:28:04 +0000

    updated the menu page responsiveness

--- src/pages/MenuPage.jsx
+++ src/pages/MenuPage.jsx
@@ -15,14 +15,14 @@ function MenuPage() {
   }, []);
 
   return (
-    <div className="md:grid md:grid-cols-3 sm:grid-cols-2 gap-5 px-[10%]">
+    <div className="w-fit mx-auto grid grid-cols-1 lg:grid-cols-3 md:grid-cols-2 justify-items-center justify-center gap-y-20 gap-x-14 mt-10 mb-5">
       {menuData.length === 0 ? (
         <p>Loading...</p>
       ) : (
         menuData.map((food) => (
           <div
             key={food.id}
-            className="md:w-[300px] md:h-[300px] bg-black relative rounded-[20px] my-8 mx-8"
+            className="w-72 bg-white shadow-md rounded-xl duration-500 hover:scale-105 hover:shadow-xl"
           >
             <div className="w-full h-[80%] object-cover shadow-lg rounded-[20px]">
               <img
@@ -31,9 +31,9 @@ function MenuPage() {
                 className="w-full h-full rounded-t-lg"
               />
             </div>
-            <div className="text-white my-2 mx-7">
+            <div className="text-lg font-bold text-black truncate block capitalize">
               <h1>{food.name}</h1>
-              <p>
+              <p className="text-sm text-gray-600 cursor-auto ml-2">
                 <small>${food.price}</small>
               </p>
             </div>
